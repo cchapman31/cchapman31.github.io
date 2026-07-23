6955abe99843c396e75d2d9d134793c6790356bd
cchapman31 <cody@insurancesaleslab.com>
2026-07-23 17:12:37 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 /* Fill these two in after you finish the setup steps in README.md. */
 window.CIA_CONFIG = {
   // Google Cloud > Credentials > OAuth 2.0 Client ID (Web application)
-  GOOGLE_CLIENT_ID: 'PASTE_YOUR_CLIENT_ID.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '928081732334-jiet7kj4lvngncjbjot6vc541fbf9ldk.apps.googleusercontent.com',
 
   // Apps Script > Deploy > Web app > the /exec URL
-  API_URL: 'https://script.google.com/macros/s/PASTE_YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxrnPEyBvsjXmijUSQSlhkqYEgrZePIGHwY25-Is7_bUV6fet0bv8dpqdslRLZshg1l/exec',
 
   // Shown on the sign-in screen. The server enforces this too.
   ALLOWED_DOMAIN: 'topfarmersagent.com'
